bf5f3887cde6b9d441f5acf06b92b1dd6df34276
saif <saifibr2004@gmail.com>
2026-08-13 14:14:29 +0500

    by mistake change the name

--- backend/src/models/Envelope.js
+++ backend/src/models/Envelope.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const envelopeSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   name: { type: String, required: true },
-  budgetAmount: { type: Number, default: 0 }, // Target monthly budget
+  allocatedAmount: { type: Number, default: 0 }, // Target monthly budget
   currentBalance: { type: Number, default: 0 }, // Actual money inside
   isSystem: { type: Boolean, default: false } // To track the "Unallocated/Available" bucket
 });
